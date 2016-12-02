1533a9c89c4e01a0ca0ad9c186bf31b59448e77a
songhongwang <1505803262@qq.com>
2016-12-02 19:12:47 +0800

    rename package id

--- app/src/main/java/vigorous/lib/okdata/MainActivity.java
+++ app/src/main/java/vigorous/lib/okdata/MainActivity.java
@@ -1,7 +1,9 @@
-package tangdou.lib.testlibsdk;
+package vigorous.lib.okdata;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import vigorous.lib.okdata.R;
 
 public class MainActivity extends AppCompatActivity {
 
